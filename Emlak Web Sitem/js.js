1eefa2a325512fede0762b7752a9daaf3dfcfb73
Atilla Eymen Fison <atll.ymn.fsn@gmail.com>
2025-05-14 00:28:36 +0300

    Emlak Web Sitesi
    Bir Emlak Sitesinin Web Sitesi

---
+++ Emlak Web Sitem/js.js
@@ -0,0 +1,142 @@
+
+
+function f1(){
+    document.getElementsByClassName("resim")[0].src="img/ev1/evdetayı1.png";
+}
+
+function f2(){
+    document.getElementsByClassName("resim")[0].src="img/ev1/evdetayı2.png";
+}
+
+function f3(){
+    document.getElementsByClassName("resim")[0].src="img/ev1/evdetayı3.png";
+}
+
+function f4(){
+    document.getElementsByClassName("resim")[0].src="img/ev2/evdetayı1.avif";
+}
+
+function f5(){
+    document.getElementsByClassName("resim")[0].src="img/ev2/evdetayı2.png";
+}
+
+function f6(){
+    document.getElementsByClassName("resim")[0].src="img/ev2/evdetayı3.png";
+}
+function f7(){
+    document.getElementsByClassName("resim")[0].src="img/ev3/evdetayı1.png";
+}
+
+function f8(){
+    document.getElementsByClassName("resim")[0].src="img/ev3/evdetayı2.png";
+}
+
+function f9(){
+    document.getElementsByClassName("resim")[0].src="img/ev3/evdetayı3.png";
+}
+function f10(){
+    document.getElementsByClassName("resim")[0].src="img/ev4/evdetayı1.png";
+}
+
+function f11(){
+    document.getElementsByClassName("resim")[0].src="img/ev4/evdetayı2.png";
+}
+
+function f12(){
+    document.getElementsByClassName("resim")[0].src="img/ev4/evdetayı3.png";
+}
+function f13(){
+    document.getElementsByClassName("resim")[0].src="img/ev5/evdetayı1.png";
+}
+
+function f14(){
+    document.getElementsByClassName("resim")[0].src="img/ev5/evdetayı2.png";
+}
+
+function f15(){
+    document.getElementsByClassName("resim")[0].src="img/ev5/evdetayı3.png";
+}
+function f16(){
+    document.getElementsByClassName("resim")[0].src="img/ev6/evdetayı1.png";
+}
+
+function f17(){
+    document.getElementsByClassName("resim")[0].src="img/ev6/evdetayı2.png";
+}
+
+function f18(){
+    document.getElementsByClassName("resim")[0].src="img/ev6/evdetayı3.png";
+}
+
+function f19(){
+    document.getElementsByClassName("resim")[0].src="img/ev7/evdetayı1.png";
+}
+
+function f20(){
+    document.getElementsByClassName("resim")[0].src="img/ev7/evdetayı2.png";
+}
+
+function f21(){
+    document.getElementsByClassName("resim")[0].src="img/ev7/evdetayı3.png";
+}
+
+function f22(){
+    document.getElementsByClassName("resim")[0].src="img/ev8/evdetayı1.png";
+}
+
+function f23(){
+    document.getElementsByClassName("resim")[0].src="img/ev8/evdetayı2.png";
+}
+
+function f24(){
+    document.getElementsByClassName("resim")[0].src="img/ev8/evdetayı3.png";
+}
+
+
+function f25(){
+    document.getElementsByClassName("resim")[0].src="img/ev9/evdetayı1.png";
+}
+
+function f26(){
+    document.getElementsByClassName("resim")[0].src="img/ev9/evdetayı2.png";
+}
+
+function f27(){
+    document.getElementsByClassName("resim")[0].src="img/ev9/evdetayı3.png";
+}
+
+function f28(){
+    document.getElementsByClassName("resim")[0].src="img/ev10/evdetayı1.png";
+}
+
+function f29(){
+    document.getElementsByClassName("resim")[0].src="img/ev10/evdetayı2.png";
+}
+
+function f30(){
+    document.getElementsByClassName("resim")[0].src="img/ev10/evdetayı3.png";
+}
+
+function f31(){
+    document.getElementsByClassName("resim")[0].src="img/ev11/evindetayları1.png";
+}
+
+function f32(){
+    document.getElementsByClassName("resim")[0].src="img/ev11/evindetayları2.png";
+}
+
+function f33(){
+    document.getElementsByClassName("resim")[0].src="img/ev11/evindetayları3.png";
+}
+
+function f34(){
+    document.getElementsByClassName("resim")[0].src="img/ev12/evdetayı1.png";
+}
+
+function f35(){
+    document.getElementsByClassName("resim")[0].src="img/ev12/evdetayı2.png";
+}
+
+function f36(){
+    document.getElementsByClassName("resim")[0].src="img/ev12/evdetayı3.png";
+}
